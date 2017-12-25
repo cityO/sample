10d97054f304296d93399181ca3cd3c42c15c832
wangsfox <wangsfox@foxmail.com>
2017-12-25 17:50:44 +0800

    update database configreation

--- config/database.php
+++ config/database.php
@@ -16,7 +16,7 @@ return [
     */
 
 //    'default' => env('DB_CONNECTION', 'mysql'),
-    'default' => $db_config,
+    'default' => $db_config['connection'],
 
     /*
     |--------------------------------------------------------------------------
